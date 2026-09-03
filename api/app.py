0af9f2f9a9d101da3d29b3862c75b3a6ae33fdf5
TheEternalVoid-git <duck01.png@gmail.com>
2026-09-03 16:15:00 +0500

    Clean up app.py by removing comments and print
    Removed placeholder comments and print statement.

--- api/app.py
+++ api/app.py
@@ -1,7 +1,1 @@
-#placeholder.
-#@alvin pls use this format to ensure Vercel works correctly
-#@alvin ai cant work with vercel; 50mb limit :(
-#vercel also requires this format so dont change names
-#@alvin we use keyword search instead?
-#ill try to code it u can debug. i wanna improve my python. ; add comments while debugging
-print("Hello World!")
+
